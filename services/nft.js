f4a1eb35649ff76c3ee2c9fb89b974fedb3b035a
Whackur <ioumelon7@gmail.com>
2021-07-21 11:26:28 +0900

    Remove unnecessary try - catch block

--- services/nft.js
+++ services/nft.js
@@ -2,31 +2,23 @@ const fs = require('../libs/nft');
 const winston = require('../config/winston');
 
 // common functions
-const removeFile = function (path, fileName) {
-  try {
-    fs.access(path + fileName, fs.constants.F_OK, (err) => {
-      if (err) return winston.log.error('삭제할 수 없는 파일입니다');
-      fs.unlink(path + fileName, (err) =>
-        err
-          ? winston.log.error(err)
-          : winston.log.info(`${path + fileName} 를 정상적으로 삭제했습니다`),
-      );
-    });
-  } catch (e) {
-    throw e;
-  }
+const removeFile = (path, fileName) => {
+  fs.access(path + fileName, fs.constants.F_OK, (err) => {
+    if (err) return winston.log.error('삭제할 수 없는 파일입니다');
+    fs.unlink(path + fileName, (err) =>
+      err
+        ? winston.log.error(err)
+        : winston.log.info(`${path + fileName} 를 정상적으로 삭제했습니다`),
+    );
+  });
 };
 
 const readFile = async (path) =>
   new Promise((resolve, reject) => {
-    try {
-      fs.readFile(path, (err, data) => {
-        if (err) reject(err);
-        else resolve(data);
-      });
-    } catch (e) {
-      throw e;
-    }
+    fs.readFile(path, (err, data) => {
+      if (err) reject(err);
+      else resolve(data);
+    });
   });
 
 const isExist = async (req, path) => {
@@ -38,23 +30,19 @@ const isExist = async (req, path) => {
 };
 
 const addFileOnIPFS = async (req, name, file, path) => {
-  try {
-    const result = await req.ipfs.add(
-      {path: name, content: file},
-      {wrapWithDirectory: true, pin: false, cidVersion: 1},
-    );
-    const hash = result.cid.toString();
-    if (await isExist(req, path + result.cid.toString())) {
-      return {hash, result};
-    }
-    await req.ipfs.files.cp(
-      req.ipfsPath + result.cid.toString(),
-      path + result.cid.toString(),
-    );
+  const result = await req.ipfs.add(
+    {path: name, content: file},
+    {wrapWithDirectory: true, pin: false, cidVersion: 1},
+  );
+  const hash = result.cid.toString();
+  if (await isExist(req, path + result.cid.toString())) {
     return {hash, result};
-  } catch (e) {
-    throw e;
   }
+  await req.ipfs.files.cp(
+    req.ipfsPath + result.cid.toString(),
+    path + result.cid.toString(),
+  );
+  return {hash, result};
 };
 
 module.exports = {
