536daae4fca722df2efc3a44c0f02410642b3aea
Whackur <ioumelon7@gmail.com>
2021-07-27 16:28:02 +0900

    Modify stellar error message type

--- utils/xlm/utils.js
+++ utils/xlm/utils.js
@@ -2,6 +2,7 @@ const parseOperationError = (e) => {
   return {
     operations: e.response?.data?.extras?.result_codes?.operations,
     transaction: e.response?.data?.extras?.result_codes?.transaction,
+    message: e.message,
   };
 };
 
